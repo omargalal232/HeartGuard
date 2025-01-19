349a76ed6572b8272fe63b49697e7156567df0db
Omar Galal <125661136+omargalal232@users.noreply.github.com>
2025-01-20 01:08:16 +0200

    debug remove

--- heartguardapp05/lib/main.dart
+++ heartguardapp05/lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       initialRoute: '/login',
       routes: {
         '/login': (context) => const LoginScreen(),
